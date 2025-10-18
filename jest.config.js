2d5847396ef439ab25c44e1c002b230b410118b9
zhoujianwen <dave.zhou@55haitao.com>
2025-10-18 19:51:10 +0800

    update 2 files and create 1 file

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,33 @@
+// module.exports = {
+//   preset: 'ts-jest',
+//   testEnvironment: 'jsdom',
+//   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+//   setupFilesAfterEnv: ['@testing-library/jest-dom'],
+//   testMatch: ['**/*.test.ts', '**/*.test.tsx'],
+//   transform: {
+//     '^.+\\.(ts|tsx)$': ['ts-jest', { useESM: true }]
+//   },
+//   extensionsToTreatAsEsm: ['.ts', '.tsx'],
+//   moduleNameMapper: {
+//     '^@/(.*)$': '<rootDir>/$1' // <--- 重点：让 @/ 开头的路径别名能被 Jest 识别
+//   }
+// }
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  setupFilesAfterEnv: ['@testing-library/jest-dom'],
-  testMatch: ['**/*.test.ts', '**/*.test.tsx'],
+  preset: 'ts-jest',
   transform: {
-    '^.+\\.(ts|tsx)$': ['ts-jest', { useESM: true }]
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        tsconfig: {
+          jsx: 'react'
+        }
+      }
+    ]
   },
-  extensionsToTreatAsEsm: ['.ts', '.tsx'],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1' // <--- 重点：让 @/ 开头的路径别名能被 Jest 识别
-  }
+    '^@/(.*)$': '<rootDir>/$1'
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testMatch: ['**/__tests__/**/*.(test|spec).(ts|tsx|js)', '**/*.(test|spec).(ts|tsx|js)']
 }
